0e93b03e083bf6cec137233943f2e108126b7f4c
Shashank Mandoli <shashankmandoli2004@gmail.com>
2026-03-14 19:42:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import {Header, Footer, Home} from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      {/* <h1 className='text-orange-500 text-center bg-black text-3xl'>React Router</h1> */}
       <Header />
-      <Home />
+      <Outlet />
       <Footer />
     </>
   )
