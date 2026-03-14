14c054297299a76c8d9a5b45313450ea1f194200
Shashank Mandoli <shashankmandoli2004@gmail.com>
2026-03-14 20:20:00 +0530

    Add About.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -32,12 +32,12 @@ function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    <img className="w-96" src="https://www.pngarts.com/files/7/Online-Education-PNG-Photo.png" alt="image1" />
                 </div>
             </aside>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <img className="sm:w-96 w-48" src="https://cdni.iconscout.com/illustration/premium/thumb/education-app-6921054-5662041.png" alt="image2" />
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,10 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
+        index: true,
+        element: <Home />
+      },
+      {
         path: "home",
         element: <Home />
       },
