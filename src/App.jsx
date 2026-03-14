ae42e05aaca7b3e152a434a67ac17472d30bb698
Shashank Mandoli <shashankmandoli2004@gmail.com>
2026-03-14 19:23:36 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Header, Footer, Home} from './components'
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className='text-orange-500 text-center bg-black text-3xl'>React Router</h1> */}
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
